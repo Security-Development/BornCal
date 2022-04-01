8a7a12a282085f02a3817ce9a27dd9b31ebc1bdc
Security-Development <35417717+Security-Development@users.noreply.github.com>
2022-04-01 11:18:47 +0900

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -2,6 +2,7 @@
 #include <sys/socket.h>
 
 #define PORT 9999
+#define MAXIMUN_LENGTH 8
 
 /***
    * return sucess => 1 || faild => 0
@@ -16,13 +17,11 @@ int main() {
 	   * IPv4 Protocol => AF_INET
 	   * IPv6 Protocol => AF_INET6
 	   */
-	if( (server = socket(AF_INET, SOCK_DGRAM, 0)) == 0 )
+	if( (server = socket(AF_INET, SOCK_DGRAM, 0)) < 0 )
+	if( (connect = bind(server, (struct sockaddr *) &address, sizeof(address))) < 0 )
+	if( listen(server, MAXIMUN_LENGTH) < 0 )
 		return 0; //could not create server
-		
-	if( (connect = bind(server, (struct sockaddr *) &address, sizeof(address))) < 0 ); 
-		return 0; //could not buid server
-		
-		
+
 		
 		
 	return 1;
