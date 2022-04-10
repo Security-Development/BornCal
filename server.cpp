160b7e1ce15d3b18e5896c2847e31cd512eec7b5
Security-Development <35417717+Security-Development@users.noreply.github.com>
2022-04-10 13:26:49 +0900

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 
 #define PORT 9999
 #define MAXIMUN_LENGTH 8
@@ -11,22 +12,27 @@ using namespace std;
    * return sucess => 1 || faild => 0
    */
 int main(int argc, char const *argv[]) {
-	int server, connect;
-	struct sockaddr_in address;
-	char rBuff[MAX_BUFF] = {0}; // recive Buffer
-	
-	/***
-	   * TCP => SOCK_STREAM
-	   * UDP => SOCK_DGRAMA
-	   * IPv4 Protocol => AF_INET
-	   * IPv6 Protocol => AF_INET6
-	   */
-	if( (server = socket(AF_INET, SOCK_DGRAM, 0)) < 0 )
-	if( (connect = bind(server, (struct sockaddr *) &address, sizeof(address))) < 0 )
-	if( listen(server, MAXIMUN_LENGTH) < 0 )
-		return 0; //could not create server
+        int server, connect;
+        sockaddr_in address;
+        char rBuff[MAX_BUFF] = {0}; // recive Buffer
 
-		
-		
-	return 1;
+        /***
+           * TCP => SOCK_STREAM
+           * UDP => SOCK_DGRAMA
+           * IPv4 Protocol => AF_INET
+           * IPv6 Protocol => AF_INET6
+           */
+        if( (server = socket(AF_INET, SOCK_DGRAM, 0)) < 0 )
+        if( (connect = bind(server, (struct sockaddr *) &address, sizeof(address))) < 0 )
+        if( listen(server, MAXIMUN_LENGTH) < 0 )
+                return 0; //could not create server
+
+        // IPV4 Info Print
+        char *pIp = inet_ntoa(address.sin_addr);
+        uint16_t pPort = htons(address.sin_port);
+        printf("Suscess Create Server\nAddress : %s\nPort : %d", pIp, pPort);
+
+
+
+        return 1;
 }
