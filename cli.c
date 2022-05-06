ecc4df0cc5cf34843b9ad2468116101893e655a3
ghdwpaks <hjm7904@gmail.com>
2022-05-05 19:13:53 -0700

    Update ser.c cli.c

--- cli.c
+++ cli.c
@@ -5,51 +5,39 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-void error_handling(char *message) {
-    fputs(message,stderr);
-    fputc("\n",stderr);
-    exit(1);
-}
+void error_handling(char *message);
 
-int main(int argc, char *argv[]) {
-    int serv_sock;
-    int clnt_sock;
-
+int main(int argc, char* argv[]) {
+    int sock;
     struct sockaddr_in serv_addr;
-    struct sockaddr_in clnt_sock;
-    socklen_t clnt_addr_size;
+    char message[30];
+    int str_len;
 
-    char message[]="Hello World!";
-
-    if (argc!=2) {
-        printf("Usage : %s <port>\n",argv[0]);
+    if(argc!=3) {
+        printf("Usage : %s <IP> <port>\n",argv[0]);
         exit(1);
     }
+    sock=socket(PF_INET, SOCK_STREAM, 0);
+    if(sock==-1) error_handling("spclet() error");
 
-    serv_sock=socket(PF_INET, SOCK_STREAM, 0) ;
-    if(serv_sock == -1) error_handling("socket() error");
-
-    memset(&serv_addr,0,sizeof(serv_addr));
+    memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family=AF_INET;
-    serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
-    serv_addr.sin_port=htons(atio(argv[1]));
+    serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
+    serv_addr.sin_port=htons(atoi(argv[2]));
 
-    if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr))==-1) error_handling("bind() error");
-    if(listen(serv_sock,5)==-1) error_handling("listen() error");
+    if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1) error_handling("connect() error!");
 
-    clnt_addr_size = sizeof(clnt_addr);
-    clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr,&clnt_addr_size);
-    if(clnt_sock==-1) error_handling("accept() error");
+    str_len=read(sock, message, sizeof(message)-1);
+    if(str_len==-1) error_handling("read() error!");
 
-    write(clnt_sock, message, sizeof(message));
-    close(clnt_sock);
-    close(serv_sock);
+    printf("<essage from server: %s \n",message);
+    close(sock);
     return 0;
-
-
 
 }
 
-
-
-
+void error_handling(char *message) {
+    fputs(message,stderr);
+    fputc('\n',stderr);
+    exit(1);
+}
--- ser.c
+++ ser.c
@@ -7,7 +7,7 @@
 
 void error_handling(char *message) {
     fputs(message,stderr);
-    fputc("\n",stderr);
+    fputs("\n",stderr);
     exit(1);
 }
 
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]) {
     int clnt_sock;
 
     struct sockaddr_in serv_addr;
-    struct sockaddr_in clnt_sock;
+    struct sockaddr_in clnt_addr;
     socklen_t clnt_addr_size;
 
     char message[]="Hello World!";
@@ -32,14 +32,14 @@ int main(int argc, char *argv[]) {
     memset(&serv_addr,0,sizeof(serv_addr));
     serv_addr.sin_family=AF_INET;
     serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
-    serv_addr.sin_port=htons(atio(argv[1]));
+    serv_addr.sin_port=htons(atoi(argv[1]));
 
     if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr))==-1) error_handling("bind() error");
     if(listen(serv_sock,5)==-1) error_handling("listen() error");
 
     clnt_addr_size = sizeof(clnt_addr);
     clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr,&clnt_addr_size);
-    if(clnt_sock==-1) error_handling("accept() error");
+    if(clnt_sock == -1) error_handling("accept() error");
 
     write(clnt_sock, message, sizeof(message));
     close(clnt_sock);
