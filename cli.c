19662c73324321b4dd7ffc51e5eafa385588dabc
ghdwpaks <hjm7904@gmail.com>
2022-05-07 05:19:49 -0700

    Update ser.c cli.c rmhjm.txt for tidy up code

--- cli.c
+++ cli.c
@@ -21,7 +21,6 @@ int main(int argc, char* argv[]) {
     int count = 0;
     while (1) {
         time_t start = time(NULL);
-        //printf("start : %ld\n",start);
 
         sock=socket(PF_INET, SOCK_STREAM, 0);
         if(sock==-1) error_handling("spclet() error");
@@ -30,43 +29,27 @@ int main(int argc, char* argv[]) {
         serv_addr.sin_family=AF_INET;
         serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
         serv_addr.sin_port=htons(atoi(argv[2]));
-        int userans = 1;
 
         char message[30];
-        //printf("11\n");
-        //printf("userans : ");
-        //scanf("%d",&userans);
-        //printf("time(NULL) : %ld\n",time(NULL));
-        //printf("- : %d\n",(int)(time(NULL) - start));
         while (1) {
-
             if ((int)(time(NULL) - start) > 1 && count < 10) {
                 count++;
                 break;
             }
         }
-        if(userans == 0) {
-            break;
-        }
-
         if (connect(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1) error_handling("connect() error!");
 
-        //printf("12\n");
         str_len=read(sock, message, sizeof(message)-1);
-        //printf("13\n");
         if(str_len==-1) error_handling("read() error!");
-        //printf("14\n");
-
-        printf("<essage from server: %s \n",message);
+        printf("<Message from server: '%s'> \n",message);
         close(sock);
     }
 
     return 0;
-
 }
 
 void error_handling(char *message) {
     fputs(message,stderr);
     fputc('\n',stderr);
-    //exit(1);
+    exit(1);
 }
--- ser.c
+++ ser.c
@@ -17,77 +17,47 @@ int main(int argc, char *argv[]) {
     printf("1\n");
     int serv_sock;
     int clnt_sock;
-    //printf("2\n");
 
     struct sockaddr_in serv_addr;
     struct sockaddr_in clnt_addr;
     socklen_t clnt_addr_size;
-    //printf("3\n");
-
-    //printf("4\n");
 
     if (argc!=2) {
         printf("Usage : %s <port>\n",argv[0]);
         exit(1);
     }
-    //printf("5\n");
 
     serv_sock=socket(PF_INET, SOCK_STREAM, 0) ;
     if(serv_sock == -1) error_handling("socket() error");
-    //printf("6\n");
 
     memset(&serv_addr,0,sizeof(serv_addr));
     serv_addr.sin_family=AF_INET;
     serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
     serv_addr.sin_port=htons(atoi(argv[1]));
-    //printf("7\n");
 
     if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr))==-1) error_handling("bind() error");
 
-    //printf("8\n");
-
-    //printf("81\n");
     while (1) {
         char ti[30];
         sprintf(ti, "%f", ((float)clock()));
         char message[30]="Hello World!";
         strcat(message,ti);
-        printf("811\n");
         int userans = 1;
-        printf("812\n");
         if(listen(serv_sock,5)==-1) error_handling("listen() error");
-        printf("813\n");
         clnt_addr_size = sizeof(clnt_addr);
-        printf("814\n");
         printf("time now : %ld",clock());
         clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr,&clnt_addr_size);
-        printf("82\n");
         if(clnt_sock == -1) {
             printf("Error!");
             error_handling("accept() error");
             break;
         }
 
-
-        printf("83\n");
         write(clnt_sock, message, sizeof(message));
-
-        //printf("Enter anything for Continue :\n");
-        //scanf("%d",&userans); //scanf 를 통해 반복문에 잠시 시간을 주고 유저가 원하는 때에 다시 시작할 수 있게 해주려고 했다.
-        //하지만 scanf 문을 거치지 않고서라도, 이 곳에서 다시 continue를 해도 정상적으로 반복이 되지 않았다.
-        if (userans==0) {
-            break;
-        }
-
         close(clnt_sock);
     }
     close(serv_sock);
-    printf("0\n");
-
     return 0;
-
-
-
 }
 
 
