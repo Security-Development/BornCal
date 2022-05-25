b847d9c270b1ab2b4138771c77b187bbcc61677e
ghdwpaks <hjm7904@gmail.com>
2022-05-25 12:58:24 -0700

    Update UDP codes for explane

--- ver3/v3cli.c
+++ ver3/v3cli.c
@@ -12,40 +12,46 @@
 
 int main(int argc, char* argv[])
 {
-    int client_socket;
-    struct sockaddr_in serverAddress;
+    int sock;
+    struct sockaddr_in serv_addr;
     int server_addr_size;
     char sendBuff[BUFF_SIZE];
     char readBuff[BUFF_SIZE];
     ssize_t receivedBytes;
     ssize_t sentBytes;
-    memset(&serverAddress,0,sizeof(serverAddress));
-    serverAddress.sin_family=AF_INET;
-    inet_aton("127.0.0.1",(struct in_addr*)&serverAddress.sin_addr.s_addr);
-    serverAddress.sin_port = htons(20162);
+    memset(&serv_addr,0,sizeof(serv_addr));
+    serv_addr.sin_family=AF_INET;
+    inet_aton("127.0.0.1",(struct in_addr*)&serv_addr.sin_addr.s_addr);
+    serv_addr.sin_port = htons(20162);
     // 소켓 생성
-    if ((client_socket = socket(PF_INET,SOCK_DGRAM, 0))==-1)
+    if ((sock = socket(PF_INET,SOCK_DGRAM, 0))==-1)
         {
-        printf("socket 생성 실패\n");
+        printf("socket 생성 실패\n");
         exit(0);
         }
     while (1) {
         // 채팅 프로그램 제작
-        server_addr_size = sizeof(serverAddress);
+        server_addr_size = sizeof(serv_addr);
         //클라이언트에서 메세지 전송
         printf("클라이언트에서 보낼 말을 입력하세요 :: ");
+
         char msg[BUFF_SIZE];
         fgets(msg, BUFF_SIZE, stdin);
+        short Sequence_Number = 0;
+        short Acknowledge_Number = 0;
+        //need add codes here
         sprintf(sendBuff,"%s",msg);
-        sentBytes = sendto(client_socket,sendBuff,strlen(sendBuff),0,(struct sockaddr*)&serverAddress, sizeof(serverAddress));
-        receivedBytes = recvfrom(client_socket,readBuff,BUFF_SIZE,0,(struct sockaddr*)&serverAddress, &server_addr_size);
+        sentBytes = sendto(sock,sendBuff,strlen(sendBuff),0,(struct sockaddr*)&serv_addr, sizeof(serv_addr));
+        receivedBytes = recvfrom(sock,readBuff,BUFF_SIZE,0,(struct sockaddr*)&serv_addr, &server_addr_size);
         printf("%lu bytes read\n", receivedBytes);
+        //전송 매개 변수인 readBuff을 이렇게 정리하지 않으면 이따금씩 1바이트단위로 상대방에게 보내져버린다.
         readBuff[receivedBytes] = '\0';
-        fputs(readBuff,stdout);
+        //이 fputs 함수의 사용 의도는 정리하는것보다는 자신이 어떤 정보를 보냈는지 다시 알려주는 역할로 추정된다.
+        //fputs(readBuff,stdout);
         fflush(stdout);
         }
     // 소켓 close
-    close(client_socket);
+    close(sock);
     return 0;
 }
 
--- ver3/v3ser.c
+++ ver3/v3ser.c
@@ -12,55 +12,77 @@
 //server.cpp
 int main(int argc,char *argv[])
 {
- char readBuff[BUFFER_SIZE];
- char sendBuff[BUFFER_SIZE];
- struct sockaddr_in serverAddress, clientAddress;
- int server_fd, client_fd;
- int client_addr_size;
- ssize_t receivedBytes;
- ssize_t sentBytes;
- socklen_t clientAddressLength = 0;
- memset(&serverAddress,0,sizeof(serverAddress));
- memset(&clientAddress,0,sizeof(clientAddress));
- serverAddress.sin_family=AF_INET;
- serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
- serverAddress.sin_port = htons(20162);
- //서버소켓생성 및 서버 주소와 bind
- // 서버 소켓 생성(UDP니 SOCK_DGRAM이용)
- if ((server_fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)  //SOCK_DGRAM:UDP
-     {
+    char readBuff[BUFFER_SIZE];
+    char sendBuff[BUFFER_SIZE];
+    struct sockaddr_in serv_addr, clnt_addr;
+    int serv_sock, clnt_sock;
+    int client_addr_size;
+
+    ssize_t receivedBytes;
+    ssize_t sentBytes;
+    socklen_t clnt_addrLength = 0;
+
+    memset(&serv_addr,0,sizeof(serv_addr));//memset : 메모리를 정해진 문자로 채운다.
+    memset(&clnt_addr,0,sizeof(clnt_addr));
+    serv_addr.sin_family=AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    //ser.c : serv_addr.sin_port=htons(atoi(argv[1]));
+    serv_addr.sin_port = htons(20162); // 20162 : port number
+
+    //서버소켓생성 및 서버 주소와 bind
+    // 서버 소켓 생성(UDP니 SOCK_DGRAM을 이용)
+    if ((serv_sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1)  //SOCK_DGRAM:UDP
+    {
         printf("Sever:can not Open Socket\n");
         exit(0);
-     }
- //bind 과정
- if (bind(server_fd,(struct sockaddr *)&serverAddress,sizeof(serverAddress)) < 0)
-     {
-     printf("Server : can not bind local address");
-     exit(0);
-     }
-printf("Server:waiting connection request.\n");
- while (1) {//클라이언트 IP 확인
-     struct sockaddr_in connectSocket;
-     socklen_t connectSocketLength = sizeof(connectSocket);
-     getpeername(client_fd,(struct sockaddr*)&clientAddress,&connectSocketLength);
-     char clientIP[sizeof(clientAddress.sin_addr) + 1] = { 0 };
-     sprintf(clientIP,"%s",inet_ntoa(clientAddress.sin_addr));
-     // 접속이 안되었을 때는 while에서 출력 x
-     if(strcmp(clientIP,"0.0.0.0") != 0)
-         printf("Client:%s\n",clientIP);
-     //채팅 프로그램 제작
-     client_addr_size = sizeof(clientAddress);
-     receivedBytes = recvfrom(server_fd, readBuff, BUFF_SIZE, 0, (struct sockaddr*)&clientAddress,&client_addr_size);
-     printf("%lu bytes read\n",receivedBytes);
-     readBuff[receivedBytes]='\0';
-     fputs(readBuff,stdout);
-     fflush(stdout);
-     sprintf(sendBuff,"%s",readBuff);
-     sentBytes = sendto(server_fd, sendBuff, strlen(sendBuff), 0,(struct sockaddr*)&clientAddress, sizeof(clientAddress));
- }
- //서버 소켓 close
- close(server_fd);
- return 0;
+    }
+    //bind 과정
+    if (bind(serv_sock,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
+    {
+        printf("Server : can not bind local address");
+        exit(0);
+    }
+    printf("Server:waiting connection request.\n");
+    while (1) {//클라이언트 IP 확인
+        struct sockaddr_in connectSocket;
+        //not found object 'connectSocket' from ser.c codes
+        socklen_t connectSocketLength = sizeof(connectSocket);
+        //connectSocket 은 sockaddr_in 의 크기(Lenght)를 측정하기 위해 만들어진듯 하다.
+        getpeername(clnt_sock,(struct sockaddr*)&clnt_addr,&connectSocketLength);
+        char clientIP[sizeof(clnt_addr.sin_addr) + 1] = { 0 };
+
+        sprintf(clientIP,"%s",inet_ntoa(clnt_addr.sin_addr));
+        // 접속이 안되었을 때는 while에서 출력 x
+
+        /*
+        if(strcmp(clientIP,"0.0.0.0") != 0)
+            printf("Client:%s\n",clientIP);
+        */
+        //채팅 프로그램 제작=
+        client_addr_size = sizeof(clnt_addr);
+        //recvfrom : 수신함수
+        receivedBytes = recvfrom(serv_sock, readBuff, BUFF_SIZE, 0,
+                                 (struct sockaddr*)&clnt_addr,&client_addr_size);
+        printf("%lu bytes read 1\n",receivedBytes);
+        //ex : 5 bytes read
+
+        readBuff[receivedBytes]='\0';
+        // 상수(define)으로 정해진 BUFFER_SIZE 사용자 생성 상수에 따라 4096비트의 크기를 가진 readBuff 변수의 끝에 배열종료문자인 '\0'을 삽입함으로써 끝을 정해준다.
+        // readBuff 변수를 마무리함으로써 출력할 준비를 마친다는 뜻이다.
+        fputs(readBuff,stdout);
+        printf("%lu bytes read 2\n",receivedBytes);
+        fflush(stdout);
+
+
+        //fflush 를 통해 사용한 readBuff 변수를 다시 청소해준다.
+        sprintf(sendBuff,"%s",readBuff);
+
+        //마지막에 이 sendto 함수를 통해서 다시 값을 반환함으로써 cli가 다시금 전송을 할 수 있게 해주는거같다.
+        sentBytes = sendto(serv_sock, sendBuff, strlen(sendBuff), 0,(struct sockaddr*)&clnt_addr, sizeof(clnt_addr));
+    }
+    //서버 소켓 close
+    close(serv_sock);
+    return 0;
 }
 
 
