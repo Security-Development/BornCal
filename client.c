10f1ca30bbce3e443f4ed522c8c772f6c756a510
ghdwpaks <hjm7904@gmail.com>
2022-06-23 07:59:58 -0700

    Update server.c , client.c(little). server is ready to send result packet

--- client.c
+++ client.c
@@ -9,41 +9,67 @@ typedef struct Packet{
     char msg[50];
 } Packet;
 
+typedef struct claData {
+    double num1;
+    double num2;
+    unsigned int opNum;
+} claData;
+
+
+void error_handling(char *msg){
+    fputs(msg, stderr);
+    fputc('\n',stderr);
+    exit(1);
+}
+
 int main() {
-        int client = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP), join, size, result;
+        int client = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP), join, size, result = 0;
         char buff[50] = "12";
         struct sockaddr_in address;
         Packet packet;
+        claData cladata;
 
         if( !client )
                 return 0;
 
         memset(&address, 0, sizeof(address));
         memset(&packet, 0, sizeof(packet));
+        memset(&cladata, 0, sizeof(cladata));
 
         address.sin_family = AF_INET;
         address.sin_addr.s_addr = inet_addr("127.0.0.1");
-        address.sin_port = htons(9123);
+        //address.sin_port = htons(9123);
+        address.sin_port = htons(7904);
 
+        /*
         packet.opcode = 1;
         packet.data_len = sizeof(buff);
         strcpy(packet.msg, buff);
+        */
+        cladata.opNum = 1;
+        cladata.num1 = 50;
+        cladata.num2 = 10;
 
         if( (join = connect(client, (struct sockaddr *) &address, sizeof(address))) == -1 )
                 return 0;
 
         printf("[*] Connection Server !!\n");
 
-        sendto(client, &packet, sizeof(packet), 0, (struct sockaddr *)&address, sizeof(address));
+        //sendto(client, &packet, sizeof(packet), 0, (struct sockaddr *)&address, sizeof(address));
+        sendto(client, &cladata, sizeof(cladata), 0, (struct sockaddr *)&address, sizeof(address));
 
-        printf("[+] Send To Data Packet : 0x%x\n", packet);
+        //printf("[+] Send To Data Packet : 0x%x\n", packet);
+        printf("[+] Send To Data cladata : 0x%x\n", cladata);
+        /*
         printf("[+] opcode : %d\n", packet.opcode);
         printf("[+] data_len : %d\n", packet.data_len);
         printf("[+] msg : %s\n", packet.msg);
+        */
+        printf("[+] opNum : %d\n", cladata.opNum);
+        printf("[+] num1 : %lf\n", cladata.num1);
+        printf("[+] num2 : %lf\n", cladata.num2);
 
-        size = sizeof(address);
-        recvfrom(client, &result, sizeof(result), 0, (struct sockaddr *)&address, (int *)&size);
-        printf("[+] Server Calculated Value : %d\n", result);
+
 
 
         return 0;
--- server.c
+++ server.c
@@ -2,27 +2,26 @@
 //first version file from /ver2/u_sv.c (git commit number : c9cccf157918fc91a380594591e03a72d0a08b8f)
 
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-#include <math.h>
-
 const unsigned int header_len = sizeof(short)*2;
 
-struct Packet{
+typedef struct Packet{
     short opcode;
     short data_len;
     char msg[50];
-};
+} Packet;
 
 typedef struct claData {
     double num1;
     double num2;
     unsigned int opNum;
-}claData;
+} claData;
 
 #define MAX_MSG_SIZE 50
 
@@ -32,7 +31,7 @@ void error_handling(char *msg){
     exit(1);
 }
 
-double cal(claData cladata){		// calculate in accordance with claData
+double cal(claData cladata){            // calculate in accordance with claData
     int n_point = 1;
     switch(cladata.opNum)
     {
@@ -41,11 +40,11 @@ double cal(claData cladata){		// calculate in accordance with claData
 
         case 2:
             return cladata.num1 - cladata.num2;
-        /*
+
         case 3:
             while (1)
             {
-                if (cladata.num1 % 1.0f == 0 && cladata.num2 % 1.0f == 0)
+                if ( fmod(cladata.num1, 1.0f) == 0 && fmod(cladata.num2, 1.0f) == 0)
                     break;
                 cladata.num1 *= 10;
                 cladata.num2 *= 10;
@@ -56,19 +55,20 @@ double cal(claData cladata){		// calculate in accordance with claData
         case 4:
             while (1)
             {
-                if (cladata.num1 % 1.0f == 0 && cladata.num2 % 1.0f == 0)
+                if ( fmod(cladata.num1, 1.0f) == 0 && fmod(cladata.num2, 1.0f) == 0)
                     break;
                 cladata.num1 *= 10;
                 cladata.num2 *= 10;
                 n_point *= 100;
             }
             return cladata.num1 / cladata.num2 / n_point;
-        */
     }
 }
 
 int main(int argc, char  *argv[]){
-    printf("enterd main!");
+    printf("main entered!\n");
+    printf("log\n");
+    printf("log\n");
     int s_sock; // listening port
 
     struct sockaddr_in s_addr;  // server ip addr
@@ -76,22 +76,27 @@ int main(int argc, char  *argv[]){
     struct Packet packet;
     struct claData cladata;
     socklen_t c_addr_size;
+    printf("log\n");
 
     char msg[MAX_MSG_SIZE];
     int msg_len;
     int packet_len;
     double answer;
 
+    printf("log\n");
 
     if(argc!=2){
         printf("Usage : %s <port>\n", argv[0]);
         exit(1);
     }
 
+    printf("log\n");
     s_sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
     if(s_sock == -1)
         error_handling("socket() error");
 
+    printf("log\n");
     memset(&s_addr, 0, sizeof(s_addr)); // socketaddr_in struct initialize
     s_addr.sin_family = AF_INET;
     s_addr.sin_addr.s_addr = htonl(INADDR_ANY); // for server, localhost
@@ -99,17 +104,28 @@ int main(int argc, char  *argv[]){
 
     if(bind(s_sock, (struct sockaddr*) &s_addr, sizeof(s_addr)) == -1) // bind port & ip to socket
         error_handling("bind() error");
+    printf("log1\n");
+    while(1){   // server will not closes after client disconnect.
 
-    while(1){	// server will not closes after client disconnect.
+        printf("log2\n");
         while(1){
+            printf("log3\n");
             sleep(1);
+            printf("log4\n");
             c_addr_size = sizeof(c_addr);
+            printf("log5\n");
             memset(&cladata, 0, sizeof(cladata));
+            printf("log6\n");
             //msg_len = recvfrom(s_sock, &cladata, sizeof(cladata), 0, (struct sockaddr*)&c_addr, &c_addr_size);
+            printf("log7\n");
             packet_len = recvfrom(s_sock, &cladata, sizeof(cladata), 0, (struct sockaddr*)&c_addr, &c_addr_size);
+            printf("log8\n");
+            printf("packet_len : %d\n",packet_len);
             if (packet_len == -1 && strcmp(inet_ntoa(c_addr.sin_addr), "0.0.0.0")){
                 error_handling("recvfrom() error");
                 continue;
+            } else {
+                break;
             }
             /*
             if(!strcmp(cladata.msg,"!DISCON") || !strcmp(cladata.msg,"!EXIT")){
@@ -118,12 +134,30 @@ int main(int argc, char  *argv[]){
             printf("Message from %s:%d: %s(%d)\n", inet_ntoa(c_addr.sin_addr), ntohs(c_addr.sin_port), cladata.msg,  cladata.opcode);
              */
         }
-        printf("cladata.num1 : %lf",cladata.num1);
-        printf("cladata.num2 : %lf",cladata.num2);
-        printf("cladata.opNum : %lf",cladata.opNum);
+        printf("cladata.num1 : %lf\n",cladata.num1);
+        printf("cladata.num2 : %lf\n",cladata.num2);
+        printf("cladata.opNum : %lf\n",cladata.opNum);
 
         answer = cal(cladata);
+        printf("answer : %lf\n",answer);
+        printf("log10\n");
+        while(1) {
+            printf("log9\n");
+            sleep(1);
+            struct sockaddr_in address;
+            memset(&cladata, 0, sizeof(cladata));
+            address.sin_family = AF_INET;
+            address.sin_addr.s_addr = inet_addr("127.0.0.1");
+            address.sin_port = htons(7905);
+            cladata.opNum = 5;
+            cladata.num1 = answer;
+            cladata.num2 = answer;
+            int client = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP), join, size, result = 0;
+            if( (join = connect(client, (struct sockaddr *) &address, sizeof(address))) == -1 )
+                return 0;
+            sendto(client, &cladata, sizeof(cladata), 0, (struct sockaddr *)&address, sizeof(address));
 
+        }
         /*
         if(!strcmp(cladata.msg,"!DISCON")){
             printf("Client Disconnected: %s\n", inet_ntoa(c_addr.sin_addr));
@@ -138,3 +172,4 @@ int main(int argc, char  *argv[]){
 
     return 0;
 }
+
