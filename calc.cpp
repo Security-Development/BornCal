105e85c01155bee5c11b5883fee95762ff8c8f59
bon_4hub <73940853+kimbonsu004@users.noreply.github.com>
2022-06-24 09:29:57 +0900

    calc func refactoring done

--- calc.cpp
+++ calc.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <stdlib.h>
+#include <windows.h>
 
 #pragma warning(disable: 4996)
+
 
 using namespace std;
 
@@ -11,44 +14,48 @@ struct _clacData
 	double num2; // 숫자2 입력
 };
 
+void clear() {
+	Sleep(500);
+	system("cls");
+}
+
 int clac() {
 
 	_clacData Data;
 
 	while (1) {
-
+		clear();
 		Data = { 0,0,0 };
 
 		cout << "사용할 연산자 번호를 입력 해주세요. + - * ÷ , 1 2 3 4 : ";
 		cin >> Data.opNum;
-		if (cin.good() && (1 <= Data.opNum && Data.opNum <= 4)) {
 
-			cout << "계산할 첫 번째 값을 입력 해주세요. : ";
-			cin >> Data.num1;
-			if (cin.good()) {
-
-				cout << "계산할 두 번째 값을 입력 해주세요. : ";
-				cin >> Data.num2;
-				if (cin.good()) {
-
-					printf("%d %1f %1f \n", Data.opNum, Data.num1, Data.num2);
-				}
-				else
-					cout << "잘못된 값을 입력했습니다!! \n";
-					cin.clear();  cin.ignore(INT_MAX, '\n');
-					continue;
-			}
-			else
-				cout << "잘못된 값을 입력했습니다!! \n";
-				cin.clear();  cin.ignore(INT_MAX, '\n');
-				continue;
-		}
-		else
+		if ( !(cin.good() && (1 <= Data.opNum && Data.opNum <= 4)) ) {
 			cout << "잘못된 값을 입력했습니다!! \n";
 			cin.clear();  cin.ignore(INT_MAX, '\n');
 			continue;
+		}
 
-		break;
+		cout << "계산할 첫 번째 값을 입력 해주세요. : ";
+		cin >> Data.num1;
+
+		if ( !cin.good() ) {
+			cout << "잘못된 값을 입력했습니다!! \n";
+			cin.clear();  cin.ignore(INT_MAX, '\n');
+			continue;
+		}
+
+		cout << "계산할 두 번째 값을 입력 해주세요. : ";
+		cin >> Data.num2;
+
+		if (!cin.good()) {
+			cout << "잘못된 값을 입력했습니다!! \n";
+			cin.clear();  cin.ignore(INT_MAX, '\n');
+			continue;
+		}
+
+		printf("%d %1f %1f \n", Data.opNum, Data.num1, Data.num2);
+
 	}
 }
 
