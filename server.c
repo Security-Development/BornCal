a1daeb349b038fd7a3a3a5a412a6d8bcdd752042
ghdwpaks <hjm7904@gmail.com>
2022-06-25 23:01:24 -0700

    Completed production of the initial response part

--- server.c
+++ server.c
@@ -24,6 +24,25 @@ typedef struct claData {
     double result; // 결과값 저장
 } claData;
 
+struct _clacData
+{
+    int opNum; // + - * ÷ result srvoff, 1 2 3 4 5 6
+
+    int flagnum1;  // int, double, none 1 2 3
+    int flagnum2; // int, double, none 1 2 3
+
+    int inum1; // i숫자1
+    int inum2; // i숫자2
+
+    double dnum1;  // d숫자1
+    double dnum2; // d숫자2
+
+    int flagresult;  // int, double, none 1 2 3
+
+    int iresult; // i결과 값
+    double dresult; // d결과 값
+};
+
 #define MAX_MSG_SIZE 50
 
 void error_handling(char *msg){
@@ -74,6 +93,7 @@ int main(int argc, char  *argv[]){
         struct sockaddr_in s_addr;  // server ip addr
         struct sockaddr_in c_addr;  // clinet ip addr
         struct claData cladata;
+        struct _clacData _clacData;
         socklen_t c_addr_size;
 
         char msg[MAX_MSG_SIZE];
@@ -102,9 +122,10 @@ int main(int argc, char  *argv[]){
         while (1) {   // server will not closes after client disconnect.
             while (1) {
                 c_addr_size = sizeof(c_addr);
-                memset(&cladata, 0, sizeof(cladata));
+                //memset(&cladata, 0, sizeof(cladata));
+                memset(&_clacData, 0, sizeof(_clacData));
                 //msg_len = recvfrom(s_sock, &cladata, sizeof(cladata), 0, (struct sockaddr*)&c_addr, &c_addr_size);
-                packet_len = recvfrom(s_sock, &cladata, sizeof(cladata), 0, (struct sockaddr *) &c_addr, &c_addr_size);
+                packet_len = recvfrom(s_sock, &_clacData, sizeof(_clacData), 0, (struct sockaddr *) &c_addr, &c_addr_size);
                 if (packet_len == -1 && strcmp(inet_ntoa(c_addr.sin_addr), "0.0.0.0")) {
                     error_handling("recvfrom() error");
                     continue;
@@ -112,24 +133,36 @@ int main(int argc, char  *argv[]){
                     break;
                 }
             }
-            if (cladata.opNum == 6.0) {
+            if (_clacData.opNum == 6.0) {
                 return 0;
             }
-            answer = cal(cladata);
+            printf("packet_len : %d\n",packet_len);
+            printf("_clacData.opNum : %d\n",_clacData.opNum);
+            printf("_clacData.inum1 : %d\n",_clacData.inum1);
+            printf("_clacData.inum2 : %d\n",_clacData.inum2);
+
+            _clacData answer;
+            answer = cal(_clacData);
+
+            //a,b = cal(_clacData);
             //sleep(1);
             struct sockaddr_in address;
-            memset(&cladata, 0, sizeof(cladata));
+            memset(&_clacData, 0, sizeof(_clacData));
             address.sin_family = AF_INET;
             address.sin_addr.s_addr = inet_addr("127.0.0.1");
             address.sin_port = htons(7905);
+
+            /*
             cladata.opNum = 5;
             cladata.num1 = answer;
             cladata.num2 = answer;
             cladata.result = answer; // 결과값에 연산 결과 저장
+            */
+
             int client = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP), join, size, result = 0;
             if ((join = connect(client, (struct sockaddr *) &address, sizeof(address))) == -1)
                 return 0;
-            sendto(client, &cladata, sizeof(cladata), 0, (struct sockaddr *) &address, sizeof(address));
+            sendto(client, &answer, sizeof(answer), 0, (struct sockaddr *) &address, sizeof(address));
 
 
         }
